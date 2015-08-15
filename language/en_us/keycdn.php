e17e164e34107c73db61f3918f79cdb0866f7281
Luke Hardiman <luke@hardiman.co.nz>
2015-08-15 20:07:04 +1200

    added language
    added language file

---
+++ language/en_us/keycdn.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Language File
+ */
+// Module management
+$lang['keycdn.add_module_row'] = "Add KeyCDN";
+$lang['keycdn.module_row_plural'] = "KeyCDN";
+$lang['keycdn.name'] = "KeyCDN Service";
+?>
